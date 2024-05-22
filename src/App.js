3254571c08ee083b7e2c68bda8a0f5ca35c63a89
AMARJEET KUMAR <108997919+Amarjeet113@users.noreply.github.com>
2024-05-22 20:16:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"; 
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
